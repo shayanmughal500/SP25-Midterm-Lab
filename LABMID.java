f1ee7a01b09a4a0a679e39c022d6b4a9539b9c95
shayanmughal500 <shayanmughal500@gmail.com>
2025-05-22 13:51:55 +0500

    Lab Mid

---
+++ LABMID.java
@@ -0,0 +1,84 @@
+package lab.mid;
+import java.util.ArrayList;
+import java.util.List;
+
+interface Observer {
+    void update(double bidAmount, String bidderName);
+}
+
+
+interface Subject {
+    void registerObserver(Observer observer);
+    void removeObserver(Observer observer);
+    void notifyObservers();
+}
+
+class Auction implements Subject {
+    private String itemName;
+    private double currentBid;
+    private String highestBidder;
+    private List<Observer> observers;
+
+    public Auction(String itemName) {
+        this.itemName = itemName;
+        this.currentBid = 0.0;
+        this.highestBidder = null;
+        this.observers = new ArrayList<>();
+    }
+
+    public void registerObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(currentBid, highestBidder);
+        }
+    }
+    public void placeBid(double bidAmount, String bidderName) {
+        if (bidAmount > currentBid) {
+            currentBid = bidAmount;
+            highestBidder = bidderName;
+            System.out.println("New highest bid: " + currentBid + " by " + highestBidder);
+            notifyObservers(); 
+        } else {
+            System.out.println("Bid of " + bidAmount + " is too low, must be higher than " + currentBid);
+        }
+    }
+}
+
+class User implements Observer {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public void update(double bidAmount, String bidderName) {
+        System.out.println(name + " notified: New highest bid is " + bidAmount + " by " + bidderName);
+    }
+}
+
+public class LABMID{
+    public static void main(String[] args) {
+       
+        Auction auction = new Auction("Antique Vase");
+        User user1 = new User("Alice");
+        User user2 = new User("Bob");
+
+        
+        auction.registerObserver(user1);
+        auction.registerObserver(user2);
+
+        
+        auction.placeBid(100.0, "Alice");
+        auction.placeBid(150.0, "Bob");
+        auction.placeBid(120.0, "Alice");
+    }
+}
+
+
